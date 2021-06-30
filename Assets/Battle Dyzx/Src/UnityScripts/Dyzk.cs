122a76b36e6dd6a4e3f090ee10d45e6fe92aa13d
SuperIzzo <hristoz2004@gmail.com>
2021-06-30 13:30:45 +0100

    Add debug render and fix another scaling issue

--- Assets/Battle Dyzx/Src/UnityScripts/Dyzk.cs
+++ Assets/Battle Dyzx/Src/UnityScripts/Dyzk.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 namespace BattleDyzx
 {
     public class Dyzk : MonoBehaviour
-    {
+    {        
         public DyzkState dyzkState { get; set; }
         public Spinning spinning { get; private set; }
+        public bool debugRender;
 
         public Texture dyzkTexture
         { 
@@ -49,18 +50,31 @@ namespace BattleDyzx
         private void Update()
         {
             var normal = new Vector3(dyzkState.normal.x, dyzkState.normal.z, dyzkState.normal.y);
+            var velocity = new Vector3(dyzkState.velocity.x, dyzkState.velocity.z, dyzkState.velocity.y);
+            var control = new Vector3(dyzkState.control.x, dyzkState.control.z, dyzkState.control.y);
 
-            transform.position = new Vector3(dyzkState.position.x, dyzkState.position.z, dyzkState.position.y) + normal*10;            
+            transform.position = new Vector3(dyzkState.position.x, dyzkState.position.z, dyzkState.position.y) + normal*0.01f;            
             transform.rotation = Quaternion.FromToRotation(Vector3.up, normal);
-            spinning.RPM = dyzkState.RPM;
-
-            Debug.DrawLine(transform.position, transform.position + normal * 100, Color.red);
+            spinning.RPM = dyzkState.RPM;            
 
             var renderer = GetComponentInChildren<Renderer>();
             renderer.material.SetColor("_Color", dyzkState.isInCollision ? Color.red : Color.white);
 
             float scale = dyzkState.dyzkData.size;
             renderer.transform.localScale = new Vector3(scale, scale, scale);
+
+            if (debugRender)
+            {
+                // Draw Normal
+                Debug.DrawLine(transform.position, transform.position + normal * 100, Color.red);
+
+                // Draw Velocity
+                Debug.DrawLine(transform.position, transform.position + velocity * 100, Color.blue);
+
+                // Draw Velocity
+                Debug.DrawLine(transform.position, transform.position + control * 100, Color.cyan);
+            }
+
         }        
 
         private void OnEnable()
