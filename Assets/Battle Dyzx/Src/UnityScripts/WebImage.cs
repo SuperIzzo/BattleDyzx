64a3ee7771adf1fab279c04ea721cc082a932751
SuperIzzo <hristoz2004@gmail.com>
2021-07-04 08:08:45 +0100

    Update WebImage to use web request and move it to new scripts

---
+++ Assets/Battle Dyzx/Src/UnityScripts/WebImage.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class WebImage : MonoBehaviour {
+    [SerializeField]
+    string url;
+
+
+    IEnumerator Start()
+    {
+        var webRequest = UnityWebRequestTexture.GetTexture(url);
+        yield return webRequest.SendWebRequest();
+
+        if (webRequest.result == UnityWebRequest.Result.Success)
+        {
+            Texture2D texture = ((DownloadHandlerTexture)webRequest.downloadHandler).texture;
+
+            Image image = GetComponent<Image>();
+            image.overrideSprite = Sprite.Create(
+                texture,
+                new Rect(0, 0, texture.width, texture.height),
+                new Vector2(0.5f, 0.5f));
+        }
+    }
+}
